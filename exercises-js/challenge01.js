a2866d557fcddef983d40f34ed5edddd11709de1
Dara <daraamedeiros@outlook.com>
2021-03-01 11:12:47 -0300

    challenge 6 🚀

---
+++ exercises-js/challenge01.js
@@ -0,0 +1,22 @@
+["2020-01", "2020-02", "2021-01", "2021-02", "2019-11", "2020-09", "2022-05", "2019-12"]
+
+
+//preciso separa cada objeto, depois do - colocar uma virgula, agrupar os anos iguais com seus respectiveis meses
+
+
+outputData = [
+    {
+      year: '2021',
+      month: ['01', '02']
+    },
+    {
+      year: '2020',
+      month: ['01', '02']
+    },
+    {
+      year: '2019',
+      month: ['01', '02']
+    }
+  ];
+
+  // percorrer o array, separar por ano e mes
